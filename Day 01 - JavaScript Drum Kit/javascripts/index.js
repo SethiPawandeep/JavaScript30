cb47c5de99c81ac294d28034aedd191d11f203b7
sethipawandeep <pawandeep2007@gmail.com>
2018-08-02 13:19:29 +0530

    Initial Commit

---
+++ Day 01 - JavaScript Drum Kit/javascripts/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function (event) {
+	// const audio = document.querySelector('audio[data-key="' + event.keyCode + '"]');
+	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`)
+	if(!audio) return;
+	audio.currentTime = 0; // Revind to start
+	audio.play();
+});
