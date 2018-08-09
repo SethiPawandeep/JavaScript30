3f0962bcaf027ab80ddcb7c388081ffe0e544860
sethipawandeep <pawandeep2007@gmail.com>
2018-08-09 21:46:44 +0530

    Improved Clock

--- Day 02 - JS and CSS Clock/javascripts/script.js
+++ Day 02 - JS and CSS Clock/javascripts/script.js
@@ -2,7 +2,11 @@ const secondHand = document.querySelector('.second-hand');
 const minutesHand = document.querySelector('.min-hand');
 const hoursHand = document.querySelector('.hour-hand');
 const hand = document.querySelector('.hand');
-// const hand = 
+
+const h = document.querySelector('.hours');
+const m = document.querySelector('.minutes');
+const s = document.querySelector('.seconds');
+const t = document.querySelector('.time');
 
 function setDate() {
 	/*const secondHand = document.querySelector('.second-hand');
@@ -22,6 +26,30 @@ function setDate() {
 	const hours = now.getHours() % 12;
 	const hoursDegrees = ((hours / 12) * 360) + 90;
 	hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+	if(hours < 10) {
+		h.innerHTML = '0' + hours;
+	} else {
+		h.innerHTML = hours;
+	}
+	if(minutes < 10) {
+		m.innerHTML = '0' + minutes;
+	} else {
+		m.innerHTML = minutes;
+	}
+	if(seconds < 10) {
+		s.innerHTML = '0' + seconds;
+	} else {
+		s.innerHTML = seconds;
+	}
+	if(now.getHours() > 12) {
+		t.innerHTML = 'PM';
+	} else {
+		t.innerHTML = 'AM';
+	}
+
 }
 
 setInterval(setDate, 1000);
+
+setDate();
